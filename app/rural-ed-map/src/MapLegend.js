351757e58c49b53b32c7c6bc899aa1a074ad7791
Jon Duckworth <bigduck@gmail.com>
2017-03-05 11:53:58 -0700

    Fix legend labeling

--- app/rural-ed-map/src/MapLegend.js
+++ app/rural-ed-map/src/MapLegend.js
@@ -4,16 +4,25 @@ import * as d3 from 'd3';
 export default class MapLegend extends Component {
   render() {
     const categories = this.props.color.domain().reverse().map((x, i) => {
+      let rowLabel;
+      if (this.props.variable !== 'rural_des') {
+        const lowerLabel = (i === this.props.color.domain().length - 1) ? 0 :
+          this.props.color.domain().reverse()[i + 1];
+        rowLabel = `${this.props.formatter(lowerLabel)}- ${this.props.formatter(x)}`
+      } else {
+        rowLabel = this.props.formatter(x);
+      }
+
       return <div className="map-legend-row" key={`map-legend-row-${i}`}>
         <div
           style={{
             height: this.props.symbolHeight,
             width: this.props.symbolWidth,
-            backgroundColor: this.props.color.range()[i]
+            backgroundColor: this.props.color.range().reverse()[i]
           }}
           className="map-legend-symbol"
         />
-        <label className="map-legend-label">{this.props.formatter(x)}</label>
+        <label className="map-legend-label">{rowLabel}</label>
       </div>
     })
     return <div>
